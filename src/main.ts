fc9a3e5e8644ac8aa2b77f767d55e67f92030e76
Bohdan <brusnachenko@gmail.com>
2023-04-22 15:08:36 +0300

    changed default port

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.setGlobalPrefix("api");
-  await app.listen(3000);
+  await app.listen(5000);
 }
 
 bootstrap();
